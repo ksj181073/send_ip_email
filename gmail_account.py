1d4577c315512dbba9392268075301de0c1c8ad5
Kristian Jensen <40055790+ksj181073@users.noreply.github.com>
2022-12-27 13:46:20 +0100

    Update gmail_account.py

--- gmail_account.py
+++ gmail_account.py
@@ -1,3 +1,4 @@
 USER = ""     # First part of email address, before @, e.g. 'myname' in 'myname@gmail.com'
 PROVIDER = ""             # Last part of email address, after and including @, e.g. '@gmail.com' in 'myname@gmail.com'
 PASSWORD = ""       # Account password (app-password if using gmail)
+TO = ""
