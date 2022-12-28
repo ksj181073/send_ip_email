c1c41e307a0f63e1b9e44dbaaf5915a10e7335ea
Kristian Jensen <40055790+ksj181073@users.noreply.github.com>
2022-12-28 08:08:39 +0100

    Update send_new_ip.py
    using MIMEtext for mail message

--- send_new_ip.py
+++ send_new_ip.py
@@ -4,41 +4,42 @@ def Main():
     """
     from gmail_account import USER, PASSWORD, PROVIDER, TO
     from requests import get
-
     from smtplib import SMTP_SSL
     from ssl import create_default_context
-
     from os import path
     from datetime import datetime
+    from email.mime.text import MIMEText
 
     ip_file = "./current_ip.txt"
-
     svr = "smtp.gmail.com"
     port = "465"
-
     ip = get("https://api.ipify.org/").text
     cur_ip = ""
 
     if not path.exists(ip_file):
-        f = open(ip_file, 'w')
-        f.close()
+        with open(ip_file, 'w') as ipFile:
+            ipFile.write(ip)
+        
+        with open("ip_log", 'w') as log_file:
+            now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            log_file.write(f"{now}: Notifier started - IP is {ip}\n")
 
     with open(ip_file, 'r') as ipFile:
         cur_ip = ipFile.read()
 
     if ip != cur_ip:
-        msg = f"""Subject: new IP
-        Your IP has changed to {ip}"""
+        from_address = f"{USER}{PROVIDER}"
+        to_address = [f"{TO}+server{PROVIDER}"]
 
-        cnt = create_default_context()
+        msg = MIMEText(f"Your IP has changed to {ip}")
+        msg['subject'] = "new IP"
+        msg['from'] = from_address
+        msg['to'] = ", ".join(to_address)
         
         try:
-            with SMTP_SSL(svr, port, context=cnt) as server:
+            with SMTP_SSL(svr, port) as server:
                 server.login(user=USER, password=PASSWORD)
-                from_address = f"{USER}{PROVIDER}"
-                to_address = f"{TO}+server{PROVIDER}"
-
-                server.sendmail(from_addr=from_address, to_addrs=to_address, msg=msg)
+                server.sendmail(from_addr=from_address, to_addrs=to_address, msg=msg.as_string())
         except Exception as err:
             with open("ip_log", 'a') as log_file:
                 now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
@@ -50,6 +51,7 @@ def Main():
         with open("ip_log", 'a') as log_file:
             now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
             log_file.write(f"{now}: IP changed to {ip}\n")
+
 
 if __name__ == "__main__":
     from datetime import datetime
